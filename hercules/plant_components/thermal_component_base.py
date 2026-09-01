c632b00f4ab5caa1569135ce488173ec3310b66d
Frederik <jfrederi>
2026-09-01 15:05:30 -0600

    Ruff

--- hercules/plant_components/thermal_component_base.py
+++ hercules/plant_components/thermal_component_base.py
@@ -386,7 +386,7 @@ class ThermalComponentBase(ComponentBase):
             self.STATES.COLD_STARTING: self.cold_readying_time,
         }[self.state]
         startup_power = (self.time_in_state + delta_t - readying_time) * self.run_up_rate
-        return 0.0, min(self.P_max, max(1e-3, startup_power)) # Max power needs to be >0
+        return 0.0, min(self.P_max, max(1e-3, startup_power))  # Max power needs to be >0
 
     def step(self, h_dict):
         """Advance the thermal component simulation by one time step.
