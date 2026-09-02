bf9a10acfb69283d7b46d6c83b9804bc0b570425
misi9170 <michael.sinner@nlr.gov>
2026-09-02 15:58:31 -0600

    Sign swap for next power limits for storage components

--- hercules/hybrid_plant.py
+++ hercules/hybrid_plant.py
@@ -152,6 +152,9 @@ class HybridPlant:
             if is_storage:
                 h_dict[component_name]["power_setpoint"] = -h_dict[component_name]["power_setpoint"]
                 h_dict[component_name]["power"] = -h_dict[component_name]["power"]
+                if "power_min_next" in h_dict[component_name]:
+                    h_dict[component_name]["power_min_next"] = -h_dict[component_name]["power_min_next"]
+                    h_dict[component_name]["power_max_next"] = -h_dict[component_name]["power_max_next"]
 
         # Update the plant level outputs
         self.compute_plant_level_outputs(h_dict)
