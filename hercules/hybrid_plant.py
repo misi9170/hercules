fbfb7405136a472a07c1e0b5a51a3d47bd3c314f
misi9170 <michael.sinner@nlr.gov>
2026-09-17 09:21:48 -0600

    Correct sign flipping for min, max for storage components

--- hercules/hybrid_plant.py
+++ hercules/hybrid_plant.py
@@ -153,12 +153,11 @@ class HybridPlant:
                 h_dict[component_name]["power_setpoint"] = -h_dict[component_name]["power_setpoint"]
                 h_dict[component_name]["power"] = -h_dict[component_name]["power"]
                 if "power_min_next" in h_dict[component_name]:
-                    h_dict[component_name]["power_min_next"] = -h_dict[component_name][
-                        "power_min_next"
-                    ]
-                    h_dict[component_name]["power_max_next"] = -h_dict[component_name][
-                        "power_max_next"
-                    ]
+                    temp_max = h_dict[component_name]["power_max_next"]
+                    temp_min = h_dict[component_name]["power_min_next"]
+                    h_dict[component_name]["power_min_next"] = -temp_max
+                    h_dict[component_name]["power_max_next"] = -temp_min
+
 
         # Update the plant level outputs
         self.compute_plant_level_outputs(h_dict)
